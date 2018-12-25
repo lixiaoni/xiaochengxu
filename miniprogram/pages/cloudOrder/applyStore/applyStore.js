1aa5ae44ad87a8618b8eeda903a32e872ffb7859
jinlong.zang <502242791@qq.com>
2018-12-25 16:10:25 +0800

    storeNameExist

--- miniprogram/pages/cloudOrder/applyStore/applyStore.js
+++ miniprogram/pages/cloudOrder/applyStore/applyStore.js
@@ -130,7 +130,7 @@ Component({
         return
       }
 
-      App.http.getRequest("/api/" + name + "/exist").then(res => {
+      App.http.getRequest("/api/exist?storeName="+name).then(res => {
         if (res.obj == true) {
           wx.showToast({
             title: '名字重复，请更换店名',
